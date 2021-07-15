63992ee1c00c65a2ea3a023031ece6a053ed9b7e
marianashawky20 <86986380+marianashawky20@users.noreply.github.com>
2021-07-15 22:17:37 +0200

    Update Script.js

--- game2/Script.js
+++ game2/Script.js
@@ -3,14 +3,14 @@ var myh2 = document.getElementById("text");
 var scoretext = document.getElementById("score");
 var divstyle = document.getElementById("stylescore");
 var buttonplayin = document.getElementById("but");
-var score =0;
-var timer = 50;
+var score = 0;
+var timer = 60;
 var timeval = document.getElementById("timer");
 var timevalues = timeval.value;
 scoretext.innerHTML =  score;
 var flag = 0;
 var i = 0;
-var rightwords = ["road", "walk", "computer", "car","world", "write", "run", "since", "language", "answer"];
+var rightwords = ["road", "walk", "computer", "car", "world", "write", "run", "since", "language", "answer"];
 var wrongwords = ["odar", "kalw", "cromtuper", "rca", "dowlr", "tewir", "urn", "nices", "gangulea", "arwens"];
 
 buttonplayin.addEventListener("click", startplaying);
@@ -20,7 +20,7 @@ function startplaying() {
     myh2.innerHTML = wrongwords[i];
     var inputvalue = document.getElementById("inp");
     if (i < wrongwords.length) {
-        if (inputvalue.value == rightwords[i-1]) {
+        if (inputvalue.value == rightwords[i - 1]) {
             score++;
             scoretext.innerHTML = score;
         }
@@ -31,7 +31,7 @@ function startplaying() {
     else {
 
 
-       if(wrongwords[i]==undefined)
+        if(wrongwords[i]==undefined)
         {
             myh2.innerHTML = "End";
 
@@ -75,10 +75,11 @@ function startplaying() {
                 clearInterval(Timer);
             }
 
-            if (i == wrongwords.length+1) {
+            if (i == wrongwords.length) {
+
                 clearInterval(timerfn);
 
-                
+
             }
 
         }, 1000)
